9d75f96a80052fca0f50e32e78fd6fffbd22bd62
Flowergeoji <gyujae.kim@insertcoin.me>
2018-02-05 14:55:06 +0900

    [test] Made Test-case for SubtitlesParser

--- AVChattingPlayer/Player/SubtitlesParser.swift
+++ AVChattingPlayer/Player/SubtitlesParser.swift
@@ -8,3 +8,20 @@
 
 import Foundation
 
+public class SubtitlesParser {
+  private(set) var savedString: String?
+  private(set) var parsedPayload: [String:[[String:Any]]]?
+  private var previousReadTimeInterval: TimeInterval = 0.0
+  
+  public init(file filePath: URL, encoding: String.Encoding = String.Encoding.utf8) {
+    if let string = try? String(contentsOf: filePath, encoding: encoding) {
+      self.savedString = string
+      parsedPayload = parseSubtitles(string)
+    }
+  }
+  
+  public init(subtitles string: String) {
+    self.savedString = string
+    parsedPayload = parseSubtitles(string)
+  }
+}
--- AVChattingPlayerTests/AVChattingPlayerTests.swift
+++ AVChattingPlayerTests/AVChattingPlayerTests.swift
@@ -25,6 +25,22 @@ class AVChattingPlayerTests: XCTestCase {
   func testInitialize() {
     // Test Initialization of AVPlayer & SubtitlesParser
     
+    // 1. Initialize SubtitlesParser
+    let filePathString = "https://asset.pufflive.me/subtitles/11313/1514970441/subtitles_ec58-2018-01-03/1514966117_M4w28A.vtt"
+    guard let fileUrl: URL = URL.init(string: filePathString) else {
+      return
+    }
+    
+    let parser1: SubtitlesParser = SubtitlesParser(file: fileUrl)
+    let parser2: SubtitlesParser = SubtitlesParser(file: fileUrl, encoding: .utf8)
+    
+    XCTAssertNotNil(parser1.savedString, "SubtitlesParser should be can read VTT file")
+    XCTAssertGreaterThan((parser1.savedString?.count)!, 0, "Count of string from the vtt-file should be greated than 0")
+    XCTAssertGreaterThan((parser1.parsedPayload?.count)!, 0, "Count of subtitles should be greated than 0")
+    
+    XCTAssertNotNil(parser2.savedString, "SubtitlesParser should be can read VTT file")
+    XCTAssertGreaterThan((parser2.savedString?.count)!, 0, "Count of string from the vtt-file should be greated than 0")
+    XCTAssertGreaterThan((parser2.parsedPayload?.count)!, 0, "Count of subtitles should be greated than 0")
   }
 
   func testPerformanceExample() {
