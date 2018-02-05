23c68c754b3a82fa7b6fb5f12e8b4690fe06687b
Flowergeoji <gyujae.kim@insertcoin.me>
2018-02-05 13:51:09 +0900

    [app] Made SubtitlesParser

---
+++ AVChattingPlayer/Player/SubtitlesParser.swift
@@ -0,0 +1,10 @@
+//
+//  SubtitlesParser.swift
+//  AVChattingPlayer
+//
+//  Created by FlowerGeoji on 2018. 2. 5..
+//  Copyright © 2018년 FlowerGeoji. All rights reserved.
+//
+
+import Foundation
+
